95339852ea1888f858a018fc9c5320736bef2bee
Daniel Albinsson <daniel.albinsson@gmail.com>
2024-12-26 13:34:33 +0100

    message

--- packages/core/src/defaultCharacter.ts
+++ packages/core/src/defaultCharacter.ts
@@ -4,7 +4,7 @@ export const defaultCharacter: Character = {
     name: "Eliza",
     username: "eliza",
     plugins: [],
-    clients: [],
+    clients: ["direct"],
     modelProvider: ModelProviderName.ANTHROPIC,
     settings: {
         secrets: {},
